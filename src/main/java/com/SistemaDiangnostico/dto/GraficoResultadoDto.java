8bf2ab4f945d6017715cc152e4482573ca4996d8
rafa1 <rafa167_@hotmail.com>
2019-10-08 00:51:55 -0300

    corrigir label

--- src/main/java/com/SistemaDiangnostico/dto/GraficoResultadoDto.java
+++ src/main/java/com/SistemaDiangnostico/dto/GraficoResultadoDto.java
@@ -1,6 +1,7 @@
 package com.SistemaDiangnostico.dto;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 
 public class GraficoResultadoDto implements Serializable{
@@ -9,12 +10,42 @@ public class GraficoResultadoDto implements Serializable{
 	private static final long serialVersionUID = 1601472354660260392L;
 	
 	private List<ValorDePorcentagemDoenca> graficoDados;
+	private List<String> labels;
+	private List<Double> data;
+	
+	 
 
 	public List<ValorDePorcentagemDoenca> getGraficoDados() {
 		return graficoDados;
 	}
 
 	public void setGraficoDados(List<ValorDePorcentagemDoenca> graficoDados) {
+		this.labels =  new ArrayList<>();
+		this.data =  new ArrayList<>();
+		for (ValorDePorcentagemDoenca valorDePorcentagemDoenca : graficoDados) {
+			labels.add(valorDePorcentagemDoenca.getDoenca().getNome());
+			data.add(valorDePorcentagemDoenca.getPorcentagem());
+		}
+		
 		this.graficoDados = graficoDados;
 	}
+
+	public List<String> getLabels() {
+		return labels;
+	}
+
+	public void setLabels(List<String> labels) {
+		this.labels = labels;
+	}
+
+	public List<Double> getData() {
+		return data;
+	}
+
+	public void setData(List<Double> data) {
+		this.data = data;
+	}
+	
+	
+	
 }
